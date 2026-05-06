9e2b19e55e2ecb3963d2b4abfee5222de61e6ccf
TecnocraciaParticipativaMexico <mexico.2030.comites@gmail.com>
2026-05-06 17:42:22 -0500

    git add . git commit -m "fix: restaurar import de modulos tecnocracia" git push

--- web/app/comites/solicitar/page.tsx
+++ web/app/comites/solicitar/page.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import { modulosTecnocracia } from "../../lib/modulosTecnocracia";
 
 const modulos = modulosTecnocracia;
 
